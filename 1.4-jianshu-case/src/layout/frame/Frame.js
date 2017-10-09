4c2ee13f6198c8bf15569c1a854b88395f2dce6a
GjMagic <862635037@qq.com>
2017-10-09 23:48:15 +0800

    1.4-jianshu-case

--- 1.4-jianshu-case/src/layout/frame/Frame.js
+++ 1.4-jianshu-case/src/layout/frame/Frame.js
@@ -4,10 +4,26 @@ import Home from 'view/home/Home.js';
 import SignIn from 'view/user/SignIn';
 import SignUp from 'view/user/SignUp';
 import S from './style.scss';
+import Axios from 'axios'
+import cfg from 'config/config.json'
 
 export default class Layout extends React.Component{
     constructor(props){
         super(props);
+        this.state = {
+            myInfo: null
+        }
+    }
+
+    signInAjax() {
+        Axios
+        .post(`${cfg.url}/login`)
+        .then(res => {
+            console.log(res)
+        })
+        .catch(error => {
+            console.log(error)
+        })
     }
 
     render(){
@@ -15,8 +31,8 @@ export default class Layout extends React.Component{
             <div className={S.layout}>
                 <Nav/>
                 <Route exact path="/" component={Home}/>
-                <Route path="/sign_in" component={SignIn}/>
-                <Route path="/sign_up" component={SignUp}/>
+                <Route exact path="/sign_in" component={SignIn}/>
+                <Route exact path="/sign_up" component={SignUp}/>
             </div>
         );
     }
