c8f09894a9e1d15205ca8875928cd80d0e8bad74
GjMagic <862635037@qq.com>
2017-10-27 16:48:40 +0800

    1.5-react-dianping

--- 1.5-react-dianping/src/index.js
+++ 1.5-react-dianping/src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import App from './views/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './static/css/reset_phone.css';
+import './config/rem';
+
+ReactDOM.render(
+  <Router>
+    <Route path='/' component={App}/>
+  </Router>, 
+  document.getElementById('root')
+);
 registerServiceWorker();
