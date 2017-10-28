3efbc938a75cede60746fc8591f263a63a4b1339
GjMagic <862635037@qq.com>
2017-10-28 21:17:58 +0800

    1.5-react-dianping

--- 1.5-react-dianping/src/components/Category/Category.js
+++ 1.5-react-dianping/src/components/Category/Category.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
+
 
 import './style.less';
 
@@ -11,13 +13,52 @@ class Category extends Component {
   }
 
   componentDidMount() {
-
+    console.log()
   }
   
   render() {
     return (
-      <div>
-
+      <div className="category_wrapp clearfix">
+        <Link to="/">
+          <div></div>
+          <h3>日本菜</h3>
+        </Link>
+        <Link to="/">
+          <div></div>
+          <h3>SPA</h3>
+        </Link>
+        <Link to="/">
+          <div></div>
+          <h3>结婚</h3>
+        </Link>
+        <Link to="/">
+          <div></div>
+          <h3>学习培训</h3>
+        </Link>
+        <Link to="/">
+          <div></div>
+          <h3>西餐</h3>
+        </Link>
+        <Link to="/">
+          <div></div>
+          <h3>火车机票</h3>
+        </Link>
+        <Link to="/">
+          <div></div>
+          <h3>烧烤</h3>
+        </Link>
+        <Link to="/">
+          <div></div>
+          <h3>家装</h3>
+        </Link>
+        <Link to="/">
+          <div></div> 
+          <h3>宠物</h3>
+        </Link>
+        <Link to="/">
+          <div></div>
+          <h3>全部分类</h3>
+        </Link>
       </div>
     );
   }
