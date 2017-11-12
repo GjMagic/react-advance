c86918784695247b996c9033338b012b09b73672
GjMagic <862635037@qq.com>
2017-11-12 15:05:19 +0800

    1.6.1-redux

--- 1.6.1-redux/src/App.js
+++ 1.6.1-redux/src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
+import { addGUN, removeGUN, addGUNAsync } from './reducer';
 class App extends Component {
   render() {
-    let { store, addGUN, removeGUN, addGUNAsync } = this.props;
-    const num = store.getState();
+    let { num, addGUN, removeGUN, addGUNAsync } = this.props;
     return (
       <div>
         <h1>现在有机枪{num}把</h1>
-        <button onClick={() => { store.dispatch( addGUN() ) }}>申请武器</button>
-        <button onClick={() => { store.dispatch( removeGUN() ) }}>上交武器</button>
-        <button onClick={() => { store.dispatch( addGUNAsync() ) }}>异步申请武器</button>
+        <button onClick={addGUN}>申请武器</button>
+        <button onClick={removeGUN}>上交武器</button>
+        <button onClick={addGUNAsync}>异步申请武器</button>
       </div>
     );
   }
 }
 
-export default App
+const mapStateToProps = (state) => {
+  console.log(state)
+  return { num: state }
+}
+const mapDispatchToProps = { addGUN, removeGUN, addGUNAsync };
+export default connect(mapStateToProps, mapDispatchToProps)(App)
--- 1.6.1-redux/src/index.js
+++ 1.6.1-redux/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
 import App from './App';
-import { counter, addGUN, removeGUN, addGUNAsync } from './reducer';
+import { counter } from './reducer';
 
 const reduxDevtools = window.devToolsExtension() ? window.devToolsExtension() : () => {};
 const store = createStore(counter, compose( // compose用来组合函数
@@ -12,18 +13,9 @@ const store = createStore(counter, compose( // compose用来组合函数
   reduxDevtools
 ));
 
-function render() {
-  ReactDOM.render(
-    <App {
-      ...{
-        store, 
-        addGUN, 
-        removeGUN,
-        addGUNAsync
-      }
-    } />, 
-    document.getElementById('root')
-  )
-}
-render();
-store.subscribe(render);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  document.getElementById('root')
+)
