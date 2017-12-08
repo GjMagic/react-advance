5703989ac9ccdd8abab32a54424810a35622602c
GjMagic <862635037@qq.com>
2017-12-08 20:19:37 +0800

    1.6-react-recruit

---
+++ 1.6-react-recruit/src/component/authroute/AuthRoute.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+// 这是一个判断路由组件
+class AuthRoute extends Component {
+
+  componentDidMount() {
+    
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default AuthRoute;
--- 1.6-react-recruit/src/container/register/Register.js
+++ 1.6-react-recruit/src/container/register/Register.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
 import Logo from '../../component/logo/Logo';
+import { WingBlank, List, WhiteSpace, Button, InputItem, Radio } from 'antd-mobile';
 
 class Register extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      type: 'genius'
+    }
+  }
+  
+
   render() {
+
+    const RadioItem = Radio.RadioItem;
+
+    let { type } = this.state;
+
     return (
       <div>
         <Logo />
-        <h2>注册页</h2>
+        <WingBlank>
+          <List>
+            <InputItem>用户名：</InputItem>
+            <WhiteSpace />
+            <InputItem>密码：</InputItem>
+            <WhiteSpace />
+            <InputItem>确认密码：</InputItem>
+            <WhiteSpace />
+            <RadioItem checked={type === 'genius'}>牛人</RadioItem>
+            <RadioItem checked={type === 'boss'}>Boss</RadioItem>
+            <WhiteSpace />
+            <Button type="primary">注册</Button>            
+          </List>
+        </WingBlank>
       </div>
     );
   }
